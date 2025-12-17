8ffeca2a53cb63a1ff4a3b95dc47fbcb96dff9f0
nassipkaliv <nassipkaliv@gmail.com>
2025-12-17 22:02:40 +0500

    setters and getters, toString, hash() and equals()

--- src/Job.java
+++ src/Job.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Job {
     private String id;
     private String title;
@@ -7,16 +9,67 @@ public class Job {
     public Job(String id, String title, double budget, boolean isOpen) {
         this.id = id;
         this.title = title;
-        this.budget = budget;
+        setBudget(budget);
         this.isOpen = isOpen;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        if(budget < 0) {
+            System.out.println("Budget can not be less than 0");
+        }
+        this.budget = budget;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void close() {
+        this.isOpen = false;
+    }
+
+    public void open() {
+        this.isOpen = true;
     }
 
     @Override
     public String toString() {
-        return "Job: " +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", budget=" + budget +
-                ", isOpen=" + isOpen;
+        return "Job: {" + "id='" + id + '\'' + ", title='" + title + '\'' + ", budget=" + budget + ", isOpen=" + isOpen + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return Objects.equals(id, job.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        Job job1 = new Job("j-1", "Landing page", 200, true);
+        Job job1 = new Job("id1", "Landing page", 200, true);
+        Job job2 = new Job("id2", "Web Design", 600, false);
+        Job job3 = new Job("id1", "UI/UX Design", 999, true);
+
         System.out.println(job1);
+        System.out.println(job2);
+        System.out.println(job3);
+
+        System.out.println(job1.equals(job2));
+        System.out.println(job1.equals(job3));
+
     }
 }
 
